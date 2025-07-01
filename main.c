560e4328d1f1e3fb76e7507a6bee17820eefc8c6
P-Further-G <76175113+P-Further-G@users.noreply.github.com>
2025-07-01 22:21:04 +0300

    genel fonksiyonlar eklendi

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+
+float SEGMENTS = 10.0;
+float COEFFS[10] = {};
+
+
 /*
-* 0-1 aralığını "segments" sayısına kadar böler. Burdaki her parçaların
-* uzunluğu "segment_length" olarak geçer.
-* "no" sayısı bu parçaların kaçıncısı olduğunu söyler.
-* "x" değerinin "no" numaralı parçaya olan uzaklığına göre 0-1 arasında bir
-* değer verir.
-* Eğer "x" değeri "no" numaralı parçaya "segment_length" kadar bir mesafeden
-* daha uzaksa 0 değerini verir. Eğer daha yakınsa yakınlığına göre lineer bir
-* şekilde 1 değerine kadar artar. "x" değeri "no" numaralı parçanın üzerindeyse
-* 1 değerini verir.
-*/
+ * 0-1 aralığını "segments" sayısına kadar böler. Burdaki her parçaların
+ * uzunluğu "segment_length" olarak geçer.
+ * "no" sayısı bu parçaların kaçıncısı olduğunu söyler.
+ * "x" değerinin "no" numaralı parçaya olan uzaklığına göre 0-1 arasında bir
+ * değer verir.
+ * Eğer "x" değeri "no" numaralı parçaya "segment_length" kadar bir mesafeden
+ * daha uzaksa 0 değerini verir. Eğer daha yakınsa yakınlığına göre lineer bir
+ * şekilde 1 değerine kadar artar. "x" değeri "no" numaralı parçanın üzerindeyse
+ * 1 değerini verir.
+ */
 float image_function(float no, float segments, float x)
 {
 	float segment_length = 1/segments;
@@ -29,6 +34,51 @@ float image_function(float no, float segments, float x)
 	}
 }
 
+/*
+ * "image_function" fonksiyonunun türevi.
+ */
+float image_function_derivative(float no, float segments, float x)
+{
+	float segment_length = 1/segments;
+	float segment_no = no * segment_length;
+	if(x<=segment_no && x>=segment_no-segment_length)
+	{
+		return segments;
+	}
+	else if(x>=segment_no && x<=segment_no+segment_length)
+	{
+		return -segments;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+/*
+ * Farazi bir fonksiyon. her fonksiyonunda çalışıyor olmalı.
+ * Hangi fonksiyonlar daha hızlı çözüme götürür araştırma meselesi.
+ */
+float test_function(float x)
+{
+	return x;
+}
+
+/*
+ * "test_function" fonksiyonunun türevi. Manuel olarak giriliyor (henüz).
+ */
+float test_function_derivative(float x)
+{
+	return 1;
+}
+
+/*
+ * Kuvvet dağılımı fonksiyonu. Değiştirilebilir.
+ */
+float force_function(float x)
+{
+	return 2.0;
+}
 
 int main()
 {
